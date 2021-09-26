139c1fc09977ef62f7504bb8bfe8a3c7f7fa215e
ghonijee <akughoni@gmail.com>
2021-09-26 15:06:17 +0700

    change tailwind config & webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,56 @@
-const mix = require('laravel-mix');
+const path = require('path')
+const fs = require('fs-extra')
+const mix = require('laravel-mix')
+require('laravel-mix-versionhash')
+const tailwindcss = require('tailwindcss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 mix
-    .js('resources/js/app.js', 'public/js').vue({ version: 2 }) // Vue template
-    .postCss('resources/css/tailwind.css', 'public/css', [ // Tailwind css loaded
-        require("tailwindcss"),
-    ])
-    .sass('resources/scss/custom.scss', 'public/css'); // Custom Scss file
+  .js('resources/js/app.js', 'public/dist/js').vue()
+  .sass('resources/sass/app.scss', 'public/dist/css')
+  .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+  })
 
+  .disableNotifications()
+
+if (mix.inProduction()) {
+  mix
+    // .extract() // Disabled until resolved: https://github.com/JeffreyWay/laravel-mix/issues/1889
+    // .version() // Use `laravel-mix-versionhash` for the generating correct Laravel Mix manifest file.
+    .versionHash()
+} else {
+  mix.sourceMaps()
+}
+
+mix.webpackConfig({
+  plugins: [
+    // new BundleAnalyzerPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.json', '.vue'],
+    alias: {
+      '~': path.join(__dirname, './resources/js')
+    }
+  },
+  output: {
+    chunkFilename: 'dist/js/[chunkhash].js',
+    path: path.resolve(__dirname, mix.inProduction() ? './public/build' : './public')
+  }
+})
+
+mix.then(() => {
+  if (mix.inProduction()) {
+    process.nextTick(() => publishAseets())
+  }
+})
+
+function publishAseets () {
+  const publicDir = path.resolve(__dirname, './public')
+
+  fs.removeSync(path.join(publicDir, 'dist'))
+  fs.copySync(path.join(publicDir, 'build', 'dist'), path.join(publicDir, 'dist'))
+  fs.removeSync(path.join(publicDir, 'build'))
+}
